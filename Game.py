4cd8205df2188fc5bd21a4f0f8f8e11b6f7264f9
MatteoFioretti <mfioretti.inbox@gmail.com>
2023-11-08 21:42:57 +0100

    Added Player and Enemy classes

---
+++ Game.py
@@ -0,0 +1,12 @@
+import pygame
+from sys import exit
+from pygame.sprite import _Group
+
+class Player (pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+
+class Enemy (pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
