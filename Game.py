a8bbd0c5ca665991309e6421c30ea43a81833c1d
MatteoFioretti <mfioretti.inbox@gmail.com>
2023-11-08 22:06:04 +0100

    event loop and QUIT event

--- Game.py
+++ Game.py
@@ -16,5 +16,17 @@ screen_wdt = 800
 screen_hgt = 400
 screen = pygame.display.set_mode((screen_wdt,screen_hgt))
 
+# Clock Setup
+clock = pygame.time.Clock()
+
+# Game
 while True:
-    continue
+
+    # Check for user's inputs
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            pygame.quit()
+            exit()
+
+   
